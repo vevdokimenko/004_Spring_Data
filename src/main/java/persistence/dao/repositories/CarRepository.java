904899d0723d39acb366d3dd433ea51a4ea38ca7
vevdokimenko <evdokimenko@hotmail.com>
2022-02-01 20:58:15 +0200

    repository

--- src/main/java/persistence/dao/repositories/CarRepository.java
+++ src/main/java/persistence/dao/repositories/CarRepository.java
@@ -1,10 +1,14 @@
 package persistence.dao.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import persistence.model.Car;
 
 import java.util.List;
 
+@Repository
+@Transactional
 public interface CarRepository extends CrudRepository<Car, Long> {
     List<Car> findCarByMark(String mark);
     List<Car> findCarByMarkAndModelAndSpeed(String mark, String model, int speed);
